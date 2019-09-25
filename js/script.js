3d1ccba079d9139276edee6529e771cf66c3b5b9
SolomonEjukorlem <ejukorlemsolomon@gmail.com>
2019-09-26 00:14:21 +0100

    Third Major Commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
+
 document.querySelector('#button').addEventListener('click', bookSearch);
 
 function bookSearch(){
-    let search = document.querySelector('#search').value;
-    let searchResults = document.querySelector("#searchResults").textContent="";
-   let url = "https://www.googleapis.com/books/v1/volumes?q=search"
+   
+    
+    let searchData = document.querySelector('#search').value;
+    let searchResults = document.querySelector("#searchResults");   
+    let myHeader =  document.querySelector("#header");   
+   
+   let url = "https://www.googleapis.com/books/v1/volumes?q=" + searchData;
+   myHeader.style.display = "none";
    fetch(url)
-   .then(function(response){
-       return response.json();
+   .then((response) =>  response.json())
+   .then((data) => {
+        let result = `<h2  class ="hide">items</h2>`;
+        data.items.forEach(function(item){
+            result += `
+            <div id='card'>
+                        <h3>Title: ${item.volumeInfo.title}</h3>
+                        <p><img  src = ${item.volumeInfo.imageLinks.thumbnail}></p>
+                        <h5>Author(s): ${item.volumeInfo.authors}</h5>
+                        <h5>Publisher: ${item.volumeInfo.publisher} </h5>                                                                  
+                        <h5>ISBN: ${item.volumeInfo.industryIdentifiers[1].identifier} </h5>                         
+                        <a href=${item.volumeInfo.infoLink}><button  id ='view'>Browse</button></a>                                                                                     
+            </div> 
+            `;
+        });
+       
+        document.querySelector('#searchResults').innerHTML = result;
+        
+        document.getElementById("view").addEventListener("click", function(){
+            console.log(isbn);
+          });
    })
-   .then(function(data){
-       console.log(data) ;
-   });
+    .catch(error =>{
+        console.log("Something went wrong!");
+    });
+    
 }
+ 
 
